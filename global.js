0351986e36027fa6b52cbafde75b0adfe19f67ce
Totoro700 <ianfrancisko@gmail.com>
2023-06-05 18:20:31 +0900

    js addition, contact page edit, navbar image replaced with normal text

---
+++ global.js
@@ -0,0 +1,9 @@
+window.onload = function WindowLoad(event) {
+    var link = document.querySelector("link[rel~='icon']");
+    if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.head.appendChild(link);
+    }
+    link.href = 'resc/icon.png';
+}
