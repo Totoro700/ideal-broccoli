8f472e1e62a79770e73cb7db8784b938ce1bc1dc
Totoro700 <ianfrancisko@gmail.com>
2023-06-13 20:47:22 +0900

    base for js newproduct

---
+++ products.js
@@ -0,0 +1,8 @@
+const container = document.getElementById("container");
+
+const newProduct = document.createElement("div");
+
+function newProduct(name, description) {
+    newProduct.classList.add("product");
+    parentElement.insertBefore(newProduct, container.childNodes[positionIndex]);
+}
