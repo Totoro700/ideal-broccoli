eb9b62d52195e765e6b5fb1e6be7abf5d3de258d
Totoro700 <ianfrancisko@gmail.com>
2023-06-20 01:12:47 +0900

    backend

--- ideal-broccoli-backend/src/main/java/skill/issue/idealbroccoli/api/v1/endpoints/ItemList.java
+++ ideal-broccoli-backend/src/main/java/skill/issue/idealbroccoli/api/v1/endpoints/ItemList.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import javax.script.*;
 
 public class ItemList {
     private static HashMap<String, Integer> items = new HashMap<>();
@@ -23,14 +24,27 @@ public class ItemList {
             e.printStackTrace();
         }
     }
+
     private static String getItems() {
         StringBuilder builder = new StringBuilder();
         for(String item : items.keySet()) {
             builder.append(item).append(" ").append(items.get(item)).append("\n");
+            //i think make new product here
         }
         return builder.toString();
     }
+
     public static String getAllItems() {
         return getItems();
+    }
+
+    public static void product(String name) throws Exception {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("javascript");
+        engine.eval(new java.io.FileReader("../../../products.js"));
+        Invocable invocable = (Invocable) engine;
+        Object jsObject = engine.get("NewProduct");
+        invocable.invokeMethod(jsObject, "call", "name", "69.69", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        //NewProduct(name, price, url)
     }
 }
