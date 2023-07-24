a5f31a65a8103f5e44c7abe22551c9f753b559b0
Totoro700 <ianfrancisko@gmail.com>
2023-07-23 21:16:21 -0500

    minify inactive js files

--- js/global.js
+++ js/global.js
@@ -1,10 +1,1 @@
-window.onload = function WindowLoad(event) { // icon
-    var link = document.querySelector("link[rel~='icon']");
-    if (!link) {
-        link = document.createElement('link');
-        link.rel = 'icon';
-        document.head.appendChild(link);
-    }
-    link.href = 'resc/icon.png';
-}
-var d=document,p="property",c="content",a=d.createElement("meta");a.setAttribute(p,"og:title"),a.setAttribute(c,"Ideal Broccoli");var b=d.createElement("meta");b.setAttribute(p,"og:description"),b.setAttribute(c,"Ideal Broccoli is an innovative e-commerce platform that offers a wide range of cooking products.");var h=d.getElementsByTagName("head")[0];h.appendChild(a);h.appendChild(b);
+window.onload=function e(t){var r=document.querySelector("link[rel~='icon']");r||((r=document.createElement("link")).rel="icon",document.head.appendChild(r)),r.href="resc/icon.png"};var d=document,p="property",c="content",a=d.createElement("meta");a.setAttribute(p,"og:title"),a.setAttribute(c,"Ideal Broccoli");var b=d.createElement("meta");b.setAttribute(p,"og:description"),b.setAttribute(c,"Ideal Broccoli is an innovative e-commerce platform that offers a wide range of cooking products.");var h=d.getElementsByTagName("head")[0];h.appendChild(a),h.appendChild(b);
